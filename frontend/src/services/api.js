bc5d192aade3edb4c50abb6630c97c9d8887a395
dimas jk <dijekarim@gmail.com>
2025-09-27 05:35:01 +0000

    fix: frontend api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1", // Rails backend
+  baseURL: "http://backend:4000/api/v1", // Rails backend
   headers: {
     "Content-Type": "application/json",
   },
